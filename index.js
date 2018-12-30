76d1d7ac38caf82de556b573f198a0e925fbd45a
Katie Blizzard <kblizzard11@gmail.com>
2018-12-30 18:33:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ Variables defined in the test suite:
 - oldID
 - currentAge
 
+
 You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+let newID = oldID + 1000000000;
+ageIsValid = Number.isInteger(currentAge)
